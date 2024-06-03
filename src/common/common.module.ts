f76f7b32d79fd9083def51c317ac9e2f06f44715
laludonisetiawan <laludonisetiawan@gmail.com>
2024-06-03 23:13:49 +0800

    feat: setup common module:
    - Integrasi winston logger dengan prisma service
    	|
    	+ Mengimpor dan mengonfigurasi Winston logger di PrismaService
    	+ Menambahkan logging untuk event Prisma info, warn, error, query.
    	+ Menyertakan informasi logging detail untuk setiap event Prisma
    
    - Menambahkan layanan validasi menggunakan zoz
    	|
    	+ Menambahkan layanan validasi baru untuk menangani validasi data menggunakan Zod.
    
    - menambahkan modul umum dengan layanan konfigurasi, logging, Prisma, dan validasi
    	|
    	+ Menambahkan CommonModule yang menyediakan layanan global untuk konfigurasi aplikasi, logging dengan winston, prisma service, dan validation service.

---
+++ src/common/common.module.ts
@@ -0,0 +1,23 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import { PrismaService } from './prisma.service';
+import { ValidationService } from './validation.service';
+
+@Global()
+@Module({
+  imports: [
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      transports: [new winston.transports.Console()],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  providers: [PrismaService, ValidationService],
+  exports: [PrismaService, ValidationService],
+})
+@Module({})
+export class CommonModule {}
